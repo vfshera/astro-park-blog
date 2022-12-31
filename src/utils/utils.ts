52d25c011647b56c9cf73eb08ca3deb22ab91fc2
Franklin Shera <fshera96@gmail.com>
2022-12-31 17:28:29 +0300

    posts with slug

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,10 +2,30 @@ import { SITE } from "@data/config";
 
 // set page title
 export function setTitle(title: string) {
-  return title === "" ? SITE.name : SITE.name + " - " + title;
+  return title === "" ? SITE.name : title + " - " + SITE.name;
 }
 
 //set page sescription
 export function setDescription(desc: string) {
   return desc === "" ? SITE.description : desc;
+}
+
+//Slugify from : https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
+export function slugify(text: string): string {
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
+
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(p, (c) => b.charAt(a.indexOf(c)))
+    .replace(/&/g, "-and-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
 }
