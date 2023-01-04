cb5f66843021040bb0e64efd1d538f272145e1e8
Franklin Shera <fshera96@gmail.com>
2023-01-05 01:41:54 +0300

    newsletter

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,7 +1,17 @@
 import type { APIRoute } from "astro";
 
-export const post: APIRoute = ({ request }) => {
-  console.log(request.body);
+export const post: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
+
+  if (url.pathname !== "/api/newsletter") {
+    //invalid request origin
+    return {
+      body: JSON.stringify({
+        message: "Hmm",
+      }),
+    };
+  }
+
   return {
     body: JSON.stringify({
       message: request.body,
