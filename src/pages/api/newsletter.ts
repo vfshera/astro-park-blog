59a70ad78735824750dfbbd4ea7ce68ffabf6257
Franklin Shera <fshera96@gmail.com>
2023-01-06 03:24:20 +0300

    fix : newsletter api

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,13 +1,6 @@
 import type { APIRoute } from "astro";
 
 export const post: APIRoute = async ({ request }) => {
-  const url = new URL(request.url);
-
-  if (url.pathname !== "/api/newsletter") {
-    //invalid request origin
-    return new Response(null, { status: 400 });
-  }
-
   if (request.headers.get("Content-Type") === "application/json") {
     const sub: { name: string; email: string } = await request.json();
 
